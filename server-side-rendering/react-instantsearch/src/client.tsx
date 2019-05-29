1cc3ed5339c16e8748bd8de26417c2ade9b47cc4
Yannick Croissant <yannick.croissant@algolia.com>
2019-05-29 15:11:56 +0200

    feat(Server-Side Rendering): add React InstantSearch implementation (#10)

--- server-side-rendering/react-instantsearch/src/client.tsx
+++ server-side-rendering/react-instantsearch/src/client.tsx
@@ -4,4 +4,33 @@
  * See client configuration in `webpack.config.js`
  */
 
-// TODO
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { App } from './App';
+import { BrowserRouter, Route, RouteComponentProps } from 'react-router-dom';
+
+declare global {
+  interface Window {
+    __APP_INITIAL_STATE__: any;
+  }
+}
+
+interface Props {
+  resultsState: any;
+}
+
+const Root = ({ resultsState }: Props): JSX.Element => (
+  <BrowserRouter>
+    <Route
+      path="/"
+      component={(props: RouteComponentProps) => (
+        <App {...props} resultsState={resultsState} />
+      )}
+    />
+  </BrowserRouter>
+);
+
+ReactDOM.hydrate(
+  <Root resultsState={window.__APP_INITIAL_STATE__.resultsState} />,
+  document.querySelector('.container')
+);
---
+++ server-side-rendering/react-instantsearch/src/widgets/index.tsx
@@ -0,0 +1,4 @@
+export { default as SearchBox } from './SearchBox';
+export { default as Menu } from './Menu';
+export { default as Hits } from './Hits';
+export { default as InfiniteHits } from './InfiniteHits';
